48c05a0a3c80f57f6a9c19b42c8b0c7397c7ff82
Rodrigo <rodrigocirinocl@gmail.com>
2025-08-23 23:27:56 -0300

    fix: try catch no get para produtos

--- components/layout/Produto.tsx
+++ components/layout/Produto.tsx
@@ -1,18 +1,26 @@
+// app/(private)/produto/page.tsx
 import React from 'react';
 import Layout from '@/components/layout/LayoutBase';
 import ProductsList from '@/components/produtos/ProductsList';
 import { api } from '@/lib/api';
 
-
 const ProductsPage = async () => {
-    const response = await api('/products?page=1&limit=20', { method: 'GET' });
-    const products = response.data ?? [];
+    let products = [];
+
+    try {
+        const response = await api('/products?page=1&limit=20', { method: 'GET', cache: 'no-store' });
+        products = response.data ?? [];
+    } catch (err) {
+        console.error('Erro ao buscar produtos:', err);
+    }
 
     return (
         <Layout>
             <div className="mb-8">
                 <h1 className="text-2xl md:text-3xl font-bold">Seus Produtos</h1>
-                <p className="text-gray-500">Acesse e gerencie a sua lista de produtos à venda</p>
+                <p className="text-gray-500">
+                    Acesse e gerencie a sua lista de produtos à venda
+                </p>
             </div>
 
             <ProductsList initialProducts={products} />
