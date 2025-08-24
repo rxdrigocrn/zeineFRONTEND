30e5eb91819d39492f3585eaa38f99d8404bfe3b
Rodrigo <rodrigocirinocl@gmail.com>
2025-08-24 14:30:12 -0300

    fix: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,13 @@
-// middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { jwtVerify } from 'jose';
 
-const SECRET_KEY = process.env.JWT_SECRET_KEY;
-
 export async function middleware(request: NextRequest) {
     const token = request.cookies.get('access_token')?.value;
     const path = request.nextUrl.pathname;
-    console.log('Cookies recebidos:', token);
+    const SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+    console.log('JWT token recebido:', token);
 
     if (
         path.startsWith('/cover.jpg') ||
@@ -21,13 +20,13 @@ export async function middleware(request: NextRequest) {
         return NextResponse.next();
     }
 
+    if (!SECRET_KEY) throw new Error('Chave secreta JWT não configurada!');
+
     if (path.startsWith('/login') || path.startsWith('/register')) {
         if (token) {
             try {
-                if (!SECRET_KEY) throw new Error('Chave secreta JWT não configurada!');
-                const secret = new TextEncoder().encode(SECRET_KEY);
-                await jwtVerify(token, secret);
-                return NextResponse.redirect(new URL('/dashboard', request.url));
+                await jwtVerify(token, new TextEncoder().encode(SECRET_KEY));
+                return NextResponse.redirect(new URL('/dashboard', request.url), 308);
             } catch {
                 return NextResponse.next();
             }
@@ -36,17 +35,15 @@ export async function middleware(request: NextRequest) {
     }
 
     if (!token) {
-        return NextResponse.redirect(new URL('/login', request.url));
+        return NextResponse.redirect(new URL('/login', request.url), 308);
     }
 
     try {
-        if (!SECRET_KEY) throw new Error('Chave secreta JWT não configurada!');
-        const secret = new TextEncoder().encode(SECRET_KEY);
-        await jwtVerify(token, secret);
+        await jwtVerify(token, new TextEncoder().encode(SECRET_KEY));
         return NextResponse.next();
     } catch (error) {
         console.error('Falha na verificação do JWT:', error);
-        const response = NextResponse.redirect(new URL('/login', request.url));
+        const response = NextResponse.redirect(new URL('/login', request.url), 308);
         response.cookies.delete('access_token');
         return response;
     }
--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   reactStrictMode: true,
-//   env: {
-//     JWT_SECRET_KEY: process.env.JWT_SECRET,
-//   },
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'http',
-//         hostname: 'localhost',
-//         port: '5000',
-//         pathname: '/uploads/**',
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -32,6 +10,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'zeine.onrender.com',
+        port: '',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'zeinebackend.onrender.com',
         port: '',
         pathname: '/uploads/**',
       },
